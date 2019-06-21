789e61721c7bd4a6c821c52ee3288a2992ebfc37
MrCat9 <36909083+MrCat9@users.noreply.github.com>
2019-06-21 19:24:49 +0800

    Update 2_bar_3d.py

--- 2_bar_3d.py
+++ 2_bar_3d.py
@@ -11,16 +11,16 @@ import random
 def bar3d_base() -> Bar3D:
     data = [(i, j, random.randint(0, 12)) for i in range(6) for j in range(24)]
     c = (
-        Bar3D()
-        .add(
-            "",
+        Bar3D()  # 类
+        .add(  # 实例方法  # 这种写法需要在外面加 ()
+            "",  # 传参
             [[d[1], d[0], d[2]] for d in data],
             xaxis3d_opts=opts.Axis3DOpts(Faker.clock, type_="category"),
             yaxis3d_opts=opts.Axis3DOpts(Faker.week_en, type_="category"),
             zaxis3d_opts=opts.Axis3DOpts(type_="value"),
         )
-        .set_global_opts(
-            visualmap_opts=opts.VisualMapOpts(max_=20),
+        .set_global_opts(  # 实例方法
+            visualmap_opts=opts.VisualMapOpts(max_=20),  # 传参
             title_opts=opts.TitleOpts(title="Bar3D-基本示例"),
         )
     )
